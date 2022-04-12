9770321903f83934681a1cdc23211d640c74ab4e
artemofru <artemof.ru@yandex.ru>
2022-04-12 10:43:15 +0600

    I stydied a course on webpack

--- nodejs-weather/index.js
+++ nodejs-weather/index.js
@@ -3,6 +3,7 @@
  */
 const express = require('express')
 const bodyParser = require('body-parser')
+const weatherRequest = require('./requests/weather.request')
 
 const app = express()
 
@@ -11,16 +12,21 @@ app.use(express.static('public')) //статические файлы(css, ...)
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-    res.render('index') //рендерим файл с названием index
+    res.render('index', {weather: null, error: null}) //рендерим файл с названием index
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    
-    res.render('index')
+app.post('/', async (req, res) => {
+    // console.log(req.params) //отвечает за то - что идет после ? в адресной строке
+    // console.log(req.body)
+    const { city } = req.body
+
+    const { weather, error } = await weatherRequest(city)
+    console.log('Weather :', weather)
+    console.log('Error: ', error)
+    res.render('index', {weather, error})
 })
 
 app.listen(3000, () => {
     console.log('Server has started on port 3000...')
-
 })
+
